d8a0921e622ff818f7391d95b555ac7e61ab0323
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-16 15:31:01 +0300

    Add lowercase alphabet filter

---
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+result = ""
+
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+
+    new_char = chr(i)
+    result = result + new_char
+
+print(result)
