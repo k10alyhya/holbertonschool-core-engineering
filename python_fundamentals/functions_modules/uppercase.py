bb2a9d9300dde9bf3436852685d60f61a3a497c2
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-16 18:04:05 +0300

    Add uppercase string conversion

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    result = ""
+
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+            result = result + c
+        else:
+            result = result + c
+
+    print(result)
