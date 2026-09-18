e2c13082fde2120e3bbebfe8b81a95b6087af0e0
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-18 18:30:06 +0300

    Handle negative exponents

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,8 +2,13 @@
 
 def pow(a, b):
     result = 1
+    negative = b < 0
+    b = abs(b)
 
     for i in range(b):
         result = result * a
+
+    if negative:
+        return 1 / result
 
     return result
