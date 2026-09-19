1b8215b326f14acc787a1f12f77ef23aadf1f361
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-19 19:15:50 +0300

    Add list element replacement

---
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element in a list."""
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
