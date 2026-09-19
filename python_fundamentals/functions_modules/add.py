a2856d751a9626649313b214bc05475e7af6ef64
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-19 17:26:31 +0300

    Add simple function import example

---
+++ python_fundamentals/functions_modules/add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from add_0 import add
+
+def main():
+    a = 1
+    b = 2
+    x = add(a, b)
+    print("{} + {} = {}".format(a, b, x))
+
+
+if __name__ == "__main__":
+    main()
