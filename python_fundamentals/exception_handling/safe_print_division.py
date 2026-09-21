6532256626fa989594842ccb037d11641549bea1
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-21 20:42:13 +0300

    Fix pycodeatyle

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -2,11 +2,12 @@
 
 
 def safe_print_division(a, b):
+    """Safely divide two numbers and print the result."""
     result = None
 
     try:
         result = a / b
-    except:
+    except (ZeroDivisionError, TypeError):
         pass
     finally:
         print("Inside result: {}".format(result))
