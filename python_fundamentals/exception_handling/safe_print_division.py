21ba36c90e6c1d41db487cffd67013e4728fa635
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-21 20:37:34 +0300

    Safely divide two numbers and print the result.

---
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def safe_print_division(a, b):
+    result = None
+
+    try:
+        result = a / b
+    except:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
