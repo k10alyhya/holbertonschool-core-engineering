e0f7014a37eaf3f2aafea8ec1f0dd71fa8414e3b
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-22 14:10:32 +0300

    Add size validation to Square

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
