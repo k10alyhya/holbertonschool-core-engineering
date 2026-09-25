f4ad47e68c9c81d00ff00ba1f123d2ffc57ca50a
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-25 16:39:46 +0300

    Reject bool values in integer_validator

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -7,14 +7,11 @@ class BaseGeometry:
 
     def area(self):
         """Raise an exception since area is not implemented here."""
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validate that value is a positive integer."""
-
-        if not isinstance(value, int):
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
