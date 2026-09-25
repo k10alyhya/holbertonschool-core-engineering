89f7308ef78003a4ac97ef51bf3d8d9354c26676
Khaled Alyhya <khaled.alyhya10@gmail.com>
2026-09-25 10:35:12 +0300

    Add Rectangle class with width and height

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
         """Initialize a rectangle."""
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """Return the width."""
@@ -24,6 +25,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
 
         self.__width = value
+
     @property
     def height(self):
         """Return the height."""
